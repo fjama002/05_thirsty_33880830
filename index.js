822aa6bc4be2cb864f0334d7b7b46c054c6eeffa
fjama002 <fjama002@gold.ac.uk>
2025-11-12 11:52:41 +0000

    changed the port

--- index.js
+++ index.js
@@ -56,4 +56,6 @@ app.use("/", mainRoutes);
 // ================================
 
 // Launch the application and listen for incoming requests
-app.listen(port, () => console.log(`✅ Server running on port ${port}`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`✅ Server running on port ${port}`)
+);
